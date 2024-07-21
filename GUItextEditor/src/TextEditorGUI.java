14b4323cc0e9d1b4427af3395b5f79afc465df3f
Kerciu <Kerciuuu@gmail.com>
2024-07-21 11:07:16 +0200

    added tool bar and new,open,save,save as opts

--- GUItextEditor/src/TextEditorGUI.java
+++ GUItextEditor/src/TextEditorGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditorGUI extends JFrame {
     public TextEditorGUI() {
@@ -6,5 +7,47 @@ public class TextEditorGUI extends JFrame {
         setSize(480, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        addGuiComponents();
     }
+
+    private void addGuiComponents() {
+        this.addToolBar();
+    }
+
+    private void addToolBar() {
+        JToolBar toolBar = new JToolBar();
+
+        // ADD MENU BAR
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        // ADD MENUS
+        menuBar.add(addFileMenu());
+
+        add(toolBar, BorderLayout.NORTH);
+    }
+
+    private JMenu addFileMenu() {
+        JMenu fileMenu = new JMenu("File");
+
+        // "new file" functionality
+        JMenuItem newMenuItem = new JMenuItem("New");
+        fileMenu.add(newMenuItem);
+
+        // "open file" functionality
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        fileMenu.add(openMenuItem);
+
+        // "save" functionality
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        fileMenu.add(saveMenuItem);
+
+        // "save as" functionality
+        JMenuItem saveAsMenuItem = new JMenuItem("Save As");
+        fileMenu.add(saveAsMenuItem);
+
+        return fileMenu;
+    }
+
 }
