fb4049ff13e975df2ee8ac50104ccb3df98a298d
Kerciu <Kerciuuu@gmail.com>
2024-07-21 10:54:39 +0200

    initialized empty gui window

--- GUItextEditor/src/Main.java
+++ GUItextEditor/src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(new Runnable() {
+            /* used to make GUI more thread safe */
+            @Override
+            public void run() {
+                new TextEditorGUI().setVisible(true);
+            }
+        });
     }
 }
---
+++ GUItextEditor/src/TextEditorGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TextEditorGUI extends JFrame {
+    public TextEditorGUI() {
+        super("TextEditor");
+        setSize(480, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
