bf2752966863bca8e0f81f23fadf4485b81d363a
Kerciu <Kerciuuu@gmail.com>
2024-07-22 11:51:10 +0200

    created file menu executer class

--- GUItextEditor/src/FileMenuExecuter.java
+++ GUItextEditor/src/FileMenuExecuter.java
@@ -1,24 +1,84 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.util.concurrent.ExecutionException;
+import java.io.*;
 
-public class FileMenuSaver {
+public class FileMenuExecuter {
     private TextEditorGUI textEditorGUI;
     private JFileChooser fileChooser;
     private JTextArea textArea;
     private JMenu fileMenu;
     private File workingFile;
 
-    FileMenuSaver(TextEditorGUI textEditorGUI, JFileChooser fileChooser, JTextArea textArea, JMenu fileMenu, File workingFile) {
+    FileMenuExecuter(TextEditorGUI textEditorGUI, JFileChooser fileChooser, JTextArea textArea, JMenu fileMenu, File workingFile) {
         this.textEditorGUI = textEditorGUI;
         this.fileChooser = fileChooser;
         this.textArea = textArea;
         this.fileMenu = fileMenu;
         this.workingFile = workingFile;
+    }
+
+    public void newFile() {
+        JMenuItem newMenuItem = new JMenuItem("New");
+
+        // "new" resets everything
+        newMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // set header title
+                textEditorGUI.setTitle("Text Editor");
+                // set text area
+                textArea.setText("");
+                // reset working file
+                workingFile = null;
+            }
+        });
+
+        fileMenu.add(newMenuItem);
+    }
+
+    public void openFile() {
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        fileMenu.add(openMenuItem);
+
+        openMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = fileChooser.showOpenDialog(textEditorGUI);
+
+                // execute code only if user pressed save button
+                if (result != JFileChooser.APPROVE_OPTION) return;
+                try {
+                    File selectedFile = fileChooser.getSelectedFile();
+
+                    // update working file
+                    workingFile = selectedFile;
+
+                    textEditorGUI.setTitle(selectedFile.getName());
+
+                    // read the file
+                    FileReader fileReader = new FileReader(selectedFile);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                    // store the text in a text area
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String readText;
+                    while ((readText = bufferedReader.readLine()) != null) {
+                        // checks if there are lines to read in a text file
+                        stringBuilder.append(readText + "\n");
+                    }
+
+                    // update gui text area
+                    textArea.setText(stringBuilder.toString());
+
+                    bufferedReader.close();
+                    fileReader.close();
+
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        });
     }
 
     public void saveFile() {
@@ -93,5 +153,19 @@ public class FileMenuSaver {
                 }
             }
         });
+    }
+
+    public void exitFile() {
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // dispose of gui
+                textEditorGUI.dispose();
+            }
+        });
+
+        fileMenu.add(exitMenuItem);
     }
 }
