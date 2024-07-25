d766d9246f1fa5698b3deb1fc364799095bd881e
Kerciu <Kerciuuu@gmail.com>
2024-07-25 12:15:28 +0200

    added action listeners for zooming properties

--- GUItextEditor/src/main/java/com/kacperpackage/items/ZoomChooser.java
+++ GUItextEditor/src/main/java/com/kacperpackage/items/ZoomChooser.java
@@ -1,6 +1,8 @@
 package main.java.com.kacperpackage.items;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ZoomChooser {
     private JMenu zoomMenu;
@@ -10,18 +12,61 @@ public class ZoomChooser {
     }
 
     private void addZoomChooserItems() {
-        addZoomInItem();
-        addZoomOutItem();
-        addZoomRestoreItem();
+        zoomMenu.add(createZoomInItem());
+        zoomMenu.add(createZoomOutItem());
+        zoomMenu.add(createZoomRestoreItem());
     }
 
-    private void addZoomRestoreItem() {
+    private JMenuItem createZoomInItem() {
+        return new JMenuItem("Zoom In");
     }
 
-    private void addZoomOutItem() {
+    private JMenuItem createZoomOutItem() {
+        return new JMenuItem("Zoom Out");
     }
 
-    private void addZoomInItem() {
+    private JMenuItem createZoomRestoreItem() {
+        return new JMenuItem("Zoom Restore Default");
     }
 
+    private void addZoomItemFunctionality(JMenuItem zoomMenuItem) {
+        zoomMenuItem.addActionListener(
+                createAppropriateActionListener(zoomMenuItem)
+        );
+    }
+
+    private ActionListener createAppropriateActionListener(JMenuItem zoomMenuItem) {
+        return switch (zoomMenuItem.getText()) {
+            case "Zoom In" -> createZoomInActionListener(zoomMenuItem);
+            case "Zoom Out" -> createZoomOutActionListener(zoomMenuItem);
+            default -> createZoomRestoreActionListener(zoomMenuItem);
+        };
+    }
+
+    private ActionListener createZoomInActionListener(JMenuItem zoomInMenuItem) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+    }
+
+    private ActionListener createZoomOutActionListener(JMenuItem zoomOutMenuItem) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+    }
+
+    private ActionListener createZoomRestoreActionListener(JMenuItem zoomRestoreMenuItem) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+    }
 }
