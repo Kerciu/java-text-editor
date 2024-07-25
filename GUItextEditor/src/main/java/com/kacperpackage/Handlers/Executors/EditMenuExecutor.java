6c950932dc5e88526c50f867edd9abed6f2c943d
Kerciu <Kerciuuu@gmail.com>
2024-07-25 20:00:57 +0200

    improved code of edit menu class

--- GUItextEditor/src/main/java/com/kacperpackage/Handlers/Executors/EditMenuExecutor.java
+++ GUItextEditor/src/main/java/com/kacperpackage/Handlers/Executors/EditMenuExecutor.java
@@ -1,29 +1,53 @@
 package main.java.com.kacperpackage.Handlers.Executors;
 
+import main.java.com.kacperpackage.GUI.TextEditorGUI;
 import main.java.com.kacperpackage.Menus.Edit.EditMenu;
 
 import javax.swing.*;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 import javax.swing.undo.UndoManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class EditMenuExecutor implements Executor{
+    private TextEditorGUI textEditorGUI;
     private EditMenu editMenu;
+
+    // manage undo and redo
     private UndoManager undoManager;
 
-    public EditMenuExecutor(EditMenu editMenu, UndoManager undoManager) {
+    public EditMenuExecutor(TextEditorGUI textEditorGUI, EditMenu editMenu) {
+        this.textEditorGUI = textEditorGUI;
         this.editMenu = editMenu;
-        this.undoManager = undoManager;
+        undoManager = new UndoManager();
+        addEditListener();
     }
 
     @Override
     public void addMenuItems() {
-
+        editMenu.getEditMenu().add(createUndoMenuItem());
+        editMenu.getEditMenu().add(createRedoMenuItem());
     }
 
-    public void undo() {
+    public JMenuItem createUndoMenuItem() {
         JMenuItem undoMenuItem = new JMenuItem("Undo");
-        undoMenuItem.addActionListener(new ActionListener() {
+        undoMenuItem.addActionListener(
+                createUndoActionListener()
+        );
+        return undoMenuItem;
+    }
+
+    public JMenuItem createRedoMenuItem() {
+        JMenuItem redoMenuItem = new JMenuItem("Redo");
+        redoMenuItem.addActionListener(
+                createRedoActionListener()
+        );
+        return redoMenuItem;
+    }
+
+    public ActionListener createUndoActionListener() {
+        return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // if there are any edits undo them
@@ -31,20 +55,26 @@ public class EditMenuExecutor implements Executor{
                     undoManager.undo();
                 }
             }
-        });
-        editMenu.getEditMenu().add(undoMenuItem);
+        };
     }
 
-    public void redo() {
-        JMenuItem redoMenuItem = new JMenuItem("Redo");
-        redoMenuItem.addActionListener(new ActionListener() {
+    public ActionListener createRedoActionListener() {
+        return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (undoManager.canRedo()) {
                     undoManager.redo();
                 }
             }
+        };
+    }
+
+    public void addEditListener() {
+        textEditorGUI.getTextArea().getDocument().addUndoableEditListener(new UndoableEditListener() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                undoManager.addEdit(e.getEdit());
+            }
         });
-        editMenu.getEditMenu().add(redoMenuItem);
     }
 }
--- GUItextEditor/src/main/java/com/kacperpackage/Menus/Edit/EditMenu.java
+++ GUItextEditor/src/main/java/com/kacperpackage/Menus/Edit/EditMenu.java
@@ -11,19 +11,13 @@ import javax.swing.undo.UndoManager;
 public class EditMenu {
     private TextEditorGUI textEditorGUI;
     private JMenu editMenu;
-
-    // manage undo and redo
-    private UndoManager undoManager;
-
     private EditMenuExecutor editMenuExecutor;
 
     public EditMenu(TextEditorGUI textEditorGUI) {
         this.textEditorGUI = textEditorGUI;
-        this.undoManager = new UndoManager();
         editMenu = new JMenu("Edit");
-        editMenuExecutor = new EditMenuExecutor(this, undoManager);
+        editMenuExecutor = new EditMenuExecutor(textEditorGUI, this);
 
-        addEditListener();
         addUndoRedoOptions();
     }
 
@@ -32,16 +26,6 @@ public class EditMenu {
     }
 
     private void addUndoRedoOptions() {
-        editMenuExecutor.undo();
-        editMenuExecutor.redo();
-    }
-
-    public void addEditListener() {
-        textEditorGUI.getTextArea().getDocument().addUndoableEditListener(new UndoableEditListener() {
-            @Override
-            public void undoableEditHappened(UndoableEditEvent e) {
-                undoManager.addEdit(e.getEdit());
-            }
-        });
+        editMenuExecutor.addMenuItems();
     }
 }
