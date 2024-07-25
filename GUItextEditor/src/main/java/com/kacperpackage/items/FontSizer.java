44438990099102efa4f622956abd6fd84d6a387f
Kerciu <Kerciuuu@gmail.com>
2024-07-25 10:38:19 +0200

    added font size highlight and select in font size scroll pane

--- GUItextEditor/src/main/java/com/kacperpackage/items/FontSizer.java
+++ GUItextEditor/src/main/java/com/kacperpackage/items/FontSizer.java
@@ -4,10 +4,13 @@ import main.java.com.kacperpackage.GUI.TextEditorGUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class FontSizer {
     private TextEditorGUI textEditorGUI;
     private FontMenuDialog fontMenuDialog;
+    private JTextField currentFontSizeField;
 
     public FontSizer(TextEditorGUI textEditorGUI, FontMenuDialog fontMenuDialog) {
         this.textEditorGUI = textEditorGUI;
@@ -49,7 +52,7 @@ public class FontSizer {
     }
 
     private JTextField createCurrentFontSizeField() {
-        JTextField currentFontSizeField = new JTextField(
+        currentFontSizeField = new JTextField(
                 parseCurrentFontSizeToString(
                         getCurrentFontSize()
                 )
@@ -72,6 +75,9 @@ public class FontSizer {
         // from 8 to 72 with 2-sized laps
         for (int i = 8; i < 73; i += 2) {
             JLabel fontSizeValueLabel = new JLabel(Integer.toString(i));
+            fontSizeValueLabel.addMouseListener(
+                    createMouseListener(fontSizeValueLabel)
+            );
             listOfFontSizesPanel.add(fontSizeValueLabel);
         }
     }
@@ -84,5 +90,25 @@ public class FontSizer {
 
     private void initializeFontSizePanelComponents(JPanel fontSizePanel) {
         fontSizePanel.add(createCurrentFontSizeField());
+    }
+
+    private MouseAdapter createMouseListener(JLabel fontSizeValueLabel) {
+        return new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                currentFontSizeField.setText(fontSizeValueLabel.getText());
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                fontSizeValueLabel.setOpaque(true);
+                fontSizeValueLabel.setBackground(Color.PINK);
+                fontSizeValueLabel.setForeground(Color.WHITE);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                fontSizeValueLabel.setBackground(null);
+                fontSizeValueLabel.setForeground(null);
+            }
+        };
     }
 }
