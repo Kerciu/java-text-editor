ce799617f3e0aa853cdbea6f6aaa3bfe68974f57
Kerciu <Kerciuuu@gmail.com>
2024-07-25 11:39:16 +0200

    applying now works, added current font parameters container class

--- GUItextEditor/src/main/java/com/kacperpackage/items/FontMenuDialog.java
+++ GUItextEditor/src/main/java/com/kacperpackage/items/FontMenuDialog.java
@@ -11,6 +11,11 @@ import java.awt.event.MouseEvent;
 
 public class FontMenuDialog extends JDialog {
     private TextEditorGUI textEditorGUI;
+    private FontChooser fontChooser;
+    private FontStyler fontStyler;
+    private FontSizer fontSizer;
+    private FontColorer fontColorer;
+    private CurrentFontParametersContainer currentFontParametersContainer;
 
     public FontMenuDialog(TextEditorGUI textEditorGUI) {
         this.textEditorGUI = textEditorGUI;
@@ -48,27 +53,31 @@ public class FontMenuDialog extends JDialog {
     }
 
     private void addMenuComponents(JPanel fontPanel) {
+        addAllFontManagers(fontPanel);
+        addActionButtons();
+    }
+
+    private void addAllFontManagers(JPanel fontPanel) {
         addFontChooser(fontPanel);
         addFontStyler();
         addFontSizer();
         addFontColorer();
-        addActionButtons();
     }
 
     private void addFontChooser(JPanel fontPanel) {
-        new FontChooser(textEditorGUI, fontPanel);
+        fontChooser = new FontChooser(textEditorGUI, fontPanel);
     }
 
     private void addFontStyler() {
-        new FontStyler(textEditorGUI, this);
+        fontStyler = new FontStyler(textEditorGUI, this);
     }
 
     private void addFontSizer() {
-        new FontSizer(textEditorGUI, this);
+        fontSizer = new FontSizer(textEditorGUI, this);
     }
 
     private void addFontColorer() {
-        new FontColorer(textEditorGUI, this);
+        fontColorer = new FontColorer(textEditorGUI, this);
     }
 
     private void addActionButtons() {
@@ -78,12 +87,14 @@ public class FontMenuDialog extends JDialog {
 
     private JButton createApplyButton() {
         JButton applyButton = new JButton("Apply");
+        addActionToApplyButton(applyButton);
         applyButton.setBounds(230, 265, 75, 25);
         return applyButton;
     }
 
     private JButton createCancelButton() {
         JButton cancelButton = new JButton("Cancel");
+        addActionToCancelButton(cancelButton);
         cancelButton.setBounds(315, 265, 75, 25);
         return cancelButton;
     }
@@ -94,19 +105,28 @@ public class FontMenuDialog extends JDialog {
         );
     }
 
-    private ActionListener createApplyButtonActionListener(JButton applyButton) {
-        return new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        };
-    }
-
     private void addActionToCancelButton(JButton cancelButton) {
         cancelButton.addActionListener(
                 createCancelButtonActionListener(cancelButton)
         );
+    }
+
+    private ActionListener createApplyButtonActionListener(JButton applyButton) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveCurrentParametersIntoContainer();
+
+                String fontType = currentFontParametersContainer.getCurrentFontType();
+                int fontStyle = currentFontParametersContainer.getCurrentFontStyleAsInt();
+                int fontSize = currentFontParametersContainer.getCurrentFontSize();
+                Color fontColor = currentFontParametersContainer.getCurrentFontColor();
+
+                Font newFont = new Font(fontType, fontStyle, fontSize);
+                textEditorGUI.getTextArea().setFont(newFont);
+                textEditorGUI.getTextArea().setForeground(fontColor);
+            }
+        };
     }
 
     private ActionListener createCancelButtonActionListener(JButton cancelButton) {
@@ -116,5 +136,14 @@ public class FontMenuDialog extends JDialog {
 
             }
         };
+    }
+
+    private void saveCurrentParametersIntoContainer() {
+        currentFontParametersContainer = new CurrentFontParametersContainer(
+                fontChooser.getCurrentFontField().getText(),
+                fontStyler.getCurrentFontStyleField().getText(),
+                Integer.parseInt(fontSizer.getCurrentFontSizeField().getText()),
+                fontColorer.getCurrentColorBox().getBackground()
+        );
     }
 }
