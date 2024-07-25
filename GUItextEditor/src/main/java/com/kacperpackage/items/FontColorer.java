99226a3a408e275152797b05c6fa7c39a2544b1c
Kerciu <Kerciuuu@gmail.com>
2024-07-25 10:52:49 +0200

    added dialog window to enable user to choose their color

--- GUItextEditor/src/main/java/com/kacperpackage/items/FontColorer.java
+++ GUItextEditor/src/main/java/com/kacperpackage/items/FontColorer.java
@@ -3,6 +3,9 @@ package main.java.com.kacperpackage.items;
 import main.java.com.kacperpackage.GUI.TextEditorGUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FontColorer {
     private TextEditorGUI textEditorGUI;
@@ -15,6 +18,41 @@ public class FontColorer {
     }
 
     private void initializeFontSizeComponents() {
+        JPanel currentColorBox = createCurrentColorBox();
+        fontMenuDialog.add(currentColorBox);
 
+        JButton chooseColorButton = createChooseColorButton(currentColorBox);
+        fontMenuDialog.add(chooseColorButton);
     }
+
+    private JPanel createCurrentColorBox() {
+        JPanel currentColorBox = new JPanel();
+        currentColorBox.setBounds(175, 200, 23, 23);
+        currentColorBox.setBackground(textEditorGUI.getTextArea().getForeground());
+        return currentColorBox;
+    }
+
+    private JButton createChooseColorButton(JPanel currentColorBox) {
+        JButton chooseColorButton = new JButton("Choose Color");
+        chooseColorButton.setBounds(10, 200, 150, 25);
+        chooseColorButton.addActionListener(
+                createActionListener(currentColorBox)
+        );
+        return chooseColorButton;
+    }
+
+    private ActionListener createActionListener(JPanel currentColorBox) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color color = extractColorFromDialog();
+                currentColorBox.setBackground(color);
+            }
+        };
+    }
+
+    private Color extractColorFromDialog() {
+        return JColorChooser.showDialog(null, "Select a color", Color.BLACK);
+    }
+
 }
